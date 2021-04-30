09fb69ed672820b17ae1f01149a07ef542c854ca
ASUS <hamza.jouini@esprit.tn>
2021-04-30 09:06:47 +0200

    crud

--- src/Controller/SeanceController.php
+++ src/Controller/SeanceController.php
@@ -10,7 +10,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
+use Knp\Component\Pager\PaginatorInterface;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 /**
  * @Route("/seance")
  */
@@ -18,17 +20,50 @@ class SeanceController extends AbstractController
 {
     /**
      * @Route("/", name="seance_index", methods={"GET"})
+     * @param Request $request
+     * @param PaginatorInterface $paginator
+     * @return Response
      */
-    public function index(): Response
+    public function index(Request $request, PaginatorInterface $paginator): Response
     {
+
         $seances = $this->getDoctrine()
+
             ->getRepository(Seance::class)
             ->findAll();
+
+        // Retrieve the entity manager of Doctrine
+
+
+
+
+
+
+        // Paginate the results of the query
+        /** @var TYPE_NAME $paginator */
+        /** @var TYPE_NAME $request */
+
+        /** @var TYPE_NAME $appointments */
+
+
+        $seances = $paginator->paginate(
+        // Doctrine Query, not results
+            $seances,
+            // Define the page parameter
+            $request->query->getInt('page', 1),
+            // Items per page
+            3);
+
+
+
+
 
         return $this->render('seance/index.html.twig', [
             'seances' => $seances,
         ]);
     }
+
+
 
     /**
      * @Route("/new", name="seance_new", methods={"GET","POST"})
@@ -114,5 +149,7 @@ class SeanceController extends AbstractController
               'seances' => $seances,
           ]);
       }
+
+
 
 }
